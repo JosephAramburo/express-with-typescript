a4747a21c50be30aaf40d01203ef384b6b9327c7
joseph.aramburo <joseph.aramburo@JOSEPHARAMBURO>
2020-02-22 22:17:02 -0700

    [ADD] Add envirotment

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,28 @@
 import express = require('express');
+import dotenv =  require('dotenv');
+import path = require('path');
 
 interface MessageJson{
     text : string;
 }
 
 const app : express.Application = express();
+console.log(process.env.NODE_ENV);
+dotenv.config({
+    path : path.resolve(__dirname,`../.env.${process.env.NODE_ENV}`)
+});
 
 app.get('', (req, res) => {
+
+console.log(process.env.URL);
     let message : MessageJson = {
         text : 'HOLAA!'
     };
     res.json(message);
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.info(`Run server on port : 3000`);
+app.listen(PORT, () => {
+    console.info(`Run server on port : ${PORT}`);
 });
